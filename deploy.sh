d23c769dd63c347a3704b534384337ad8477a87e
LikeACodingStone <liszhouyou@gmail.com>
2025-01-24 06:56:05 +0000

    update delpoy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /home/LikeACodingStone/MINI_STUFF_STORE
+cd /home/MINISTUFF/MINI_STUFF_STORE
 git pull
+source ~/.virtualenvs/mini_stuff_store_env/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-touch /var/www/LikeACodingStone_pythonanywhere_com_wsgi.py 
+touch /var/www/MINISTUFF_pythonanywhere_com_wsgi.py 
